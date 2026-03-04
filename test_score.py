b627393ff468d24bbd73c84e35a269dc370b351b
abidmohdasif <abidmohdasif@gmail.com>
2026-03-04 13:04:34 +0000

    add score tests, all failing

---
+++ test_score.py
@@ -0,0 +1,11 @@
+def test_add_points(game,amount):
+    pass
+
+def test_apply_multiplier(game, multiplier):
+    pass
+
+def test_reset_score(game):
+    pass
+
+def test_is_high_score(game, threshold):
+    pass
