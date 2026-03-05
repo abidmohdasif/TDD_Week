7c09bff56e253fcfef75be3bcd2a0a2dff161d56
abidmohdasif <abidmohdasif@gmail.com>
2026-03-05 13:43:57 +0000

    Finished up wiht day 2

--- inventory.py
+++ inventory.py
@@ -1,20 +1,21 @@
 def add_item(inventory, item):
-    if inventory["locked"]:
+    if not item:
+        raise ValueError
+    elif len(inventory["items"]) >= inventory["capacity"]:
+        raise ValueError
+    elif not isinstance(item,str):
+        raise ValueError
+    elif inventory["locked"] == True:
         return inventory
-    if not isinstance(item,str) or item == "":
-        raise ValueError("Item has to be non empty and a string")
-    if len(inventory["items"]) >=  inventory["capacity"]:
-        raise ValueError("Inventory is full")
-    inventory["items"].append(item)
-    return inventory
-
+    else:
+        inventory["items"].append(item)
+        return inventory
 def remove_item(inventory, item):
     if inventory["locked"]:
         return inventory
-    if item not in inventory["items"]:
-        raise ValueError("Item is not in Inventory")
-    inventory["items"].remove(item)
-    return inventory
+    else:
+        inventory["items"].remove(item)
+        return inventory
 
 def get_item_count(inventory):
     return len(inventory["items"])
